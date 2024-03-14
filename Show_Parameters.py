b7beb66860b7c05bf09381663bea358312601d83
os4bim <97692941+os4bim@users.noreply.github.com>
2024-03-14 16:46:01 +0100

    Update Show_Parameters.py

--- Show_Parameters.py
+++ Show_Parameters.py
@@ -10,24 +10,32 @@ doc = __revit__.ActiveUIDocument.Document
 
 #Is element type
 
+print('#'*20)
+print('Is element type')
+print('-'*20)
+
 PA1 = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_PipeAccessory).WhereElementIsElementType().FirstElement()
 
 for p in PA1.Parameters:
-    print p.Definition.Name
+    print(p.Definition.Name)
     try:
-        print p.GUID
+        print(p.GUID)
     except:
-        print p.Definition.BuiltInParameter
-    print '-'*20
+        print(p.Definition.BuiltInParameter)
+    print('-'*20)
     
 #Is NOT element type
+
+print('#'*20)
+print('Is NOT element type')
+print('-'*20)
 
 PA2 = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_PipeAccessory).WhereElementIsNotElementType().FirstElement()
 
 for p in PA2.Parameters:
-    print p.Definition.Name
+    print(p.Definition.Name)
     try:
-        print p.GUID
+        print(p.GUID)
     except:
-        print p.Definition.BuiltInParameter
-    print '-'*20
+        print(p.Definition.BuiltInParameter)
+    print('-'*20)
